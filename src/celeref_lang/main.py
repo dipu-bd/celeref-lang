336fbd2c9301d055d45d13b65327a49eb09934c8
dipu-bd <dipu.sudipta@gmail.com>
2020-05-09 12:37:21 +0600

    Bump version

--- src/celeref_lang/main.py
+++ src/celeref_lang/main.py
@@ -11,23 +11,17 @@ parser = argparse.ArgumentParser(
     description='A versatile interpreter in python to execute programs written in JSON.'
 )
 
-parser.add_argument('--version', action='version', version='%(prog)s ' + VERSION)
-parser.add_argument('file', metavar='JSON_FILE', type=str, nargs='*',
+parser.add_argument('-v', '--version', action='version', version='%(prog)s ' + VERSION)
+parser.add_argument('file', metavar='JSON_FILE', type=str, nargs='?',
                     help='Input source file to run')
-
-parser.add_argument('-s', '--search', dest='search', type=str, nargs='*',
+parser.add_argument('-s', '--search', dest='search', type=str, nargs='?', default='',
                     help='Search list of available functions to call')
 
 
 def show_functions(query: list):
     from .functions import public_functions
     for key in sorted(public_functions.keys()):
-        exists = len(query) == 0
-        for q in query:
-            if q in key:
-                exists = True
-                break
-        if exists:
+        if not query or query in key:
             print('----- [%s] -----' % key)
             doc = public_functions[key].__doc__
             doc = '\n'.join([s.strip() for s in doc.split('\n')])
@@ -40,11 +34,11 @@ def main():
     args = parser.parse_args()
     logger.debug(args)
 
-    if args.search is not None:
+    if args.search != '':
         show_functions(args.search)
 
-    if len(args.file) > 0:
-        app = create_app(args.file[0])
+    if args.file is not None:
+        app = create_app(args.file)
         print('-' * 10)
         start_time = time.time()
         app.execute()
